9dbf98cb07f2ef2e275ede401f1febfeefe5f067
Gelis-hakutaku <153464240+Gelis-hakutaku@users.noreply.github.com>
2025-03-05 09:40:59 +0100

    Merge Projectile to Main
    * add scene for testing
    
    * adding projectiles GameObjects
    
    * add blast to explosiveProjectile
    
    * adding flammable interaction to FireProjectile
    
    There is also e "Flammable" tag to assign on the target

---
+++ Assets/Scripts/ProjectileScripts/ProjectileBlast.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class ProjectileBlast : MonoBehaviour
+{
+    [SerializeField] private Collider Collider;
+
+    [Header("BlastStats")]
+    [SerializeField] private float blastRadius = 5f;
+    [SerializeField] private float blastForce = 700f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //projette au loin tous les objets pris dans l'explosion sauf lui-même
+        foreach (Collider hit in Physics.OverlapSphere(transform.position, blastRadius))
+        {
+            if (hit.GetComponent<Rigidbody>() && hit != this.gameObject)
+            {
+                hit.GetComponent<Rigidbody>().AddForce((hit.transform.position - transform.position) * blastForce); 
+            }
+        }
+        Destroy(this.gameObject);
+    }
+}
