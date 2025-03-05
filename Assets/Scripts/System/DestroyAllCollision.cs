c0383f4fa02fde4d03616b9f5172d9aac7547946
Gelis-hakutaku <153464240+Gelis-hakutaku@users.noreply.github.com>
2025-03-05 16:22:41 +0100

    adding scoring fonction to Deadzone

--- Assets/Scripts/System/DestroyAllCollision.cs
+++ Assets/Scripts/System/DestroyAllCollision.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 public class DestroyAllCollision : MonoBehaviour
 {
     [SerializeField] private Collider _collider; //Le collider doit être set en trigger
+    [SerializeField] private PlayerScore _playerScore;
 
     private void OnTriggerEnter(Collider other)
     {
+        //script à éditer quand les objets de score seront créés
+        /*if (other.TryGetComponent<insérerClasseDeLaMortDesEnnemis>(out classe object))
+          {
+                Object.FonctionDeScore();
+                _playerScore.Score += valeurDeScore;
+          }
+        */
         Destroy(other.gameObject);
     }
 }
