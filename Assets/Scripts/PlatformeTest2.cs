bbc900d44457bdabe4f09b7b14cb6a4baad17701
EdgusLordus <louink.of.time@gmail.com>
2025-03-06 14:55:59 +0100

    Mongolfiere déplacements

---
+++ Assets/Scripts/PlatformeTest2.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlatformeTest2 : MonoBehaviour
+{
+    public float speed = 0;
+    public int valeurs = 0;
+    public GameObject[] PatrouillePoints;
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, PatrouillePoints[valeurs].transform.position, speed * Time.deltaTime);
+        transform.LookAt(PatrouillePoints[valeurs].transform);
+        if (Vector3.Distance(PatrouillePoints[valeurs].transform.position, transform.position) < 0.1)
+        {
+            if (valeurs > 2)
+            {
+                valeurs = 0;
+            }
+            else
+                valeurs++;
+            Debug.Log(valeurs);
+        }
+    }
+}
