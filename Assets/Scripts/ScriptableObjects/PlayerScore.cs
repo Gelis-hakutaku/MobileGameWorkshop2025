e7b65ca5c7787a460cc6dcb94c57f5d438a8336e
Gelis-hakutaku <153464240+Gelis-hakutaku@users.noreply.github.com>
2025-03-06 16:42:52 +0100

    Update Score.cs to add HighScore mechanic
    it need to use a save file, it is not ready yet

--- Assets/Scripts/ScriptableObjects/PlayerScore.cs
+++ Assets/Scripts/ScriptableObjects/PlayerScore.cs
@@ -1,10 +1,16 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.SceneManagement;
 
 [CreateAssetMenu(fileName = "PlayerScore", menuName = "Scriptable Objects/PlayerScore")]
 public class PlayerScore : ScriptableObject
 {
-    [SerializeField] private int _score;
+    [SerializeField] int _maxLevel;
+
+    private int _score;
+    private int[] _highscores;
 
     public int Score
     {
@@ -12,8 +18,29 @@ public class PlayerScore : ScriptableObject
         set => _score = value;
     }
 
-    void ResetScore()
+    public int[] Hishscores
+    {
+        get => _highscores;
+        set => _highscores = value;
+    }
+
+    public void Initialize()
+    {
+        _maxLevel = SceneManager.sceneCountInBuildSettings - 1;
+        _score = 0;
+        _highscores = new int[_maxLevel];
+    }
+
+    public void ResetScore()
     {
         _score = 0;
+    }
+
+    public void SetHighscore(int currentLevel)
+    {
+        if (_score > _highscores[currentLevel])
+        {
+            _highscores[currentLevel] = _score;
+        }
     }
 }
