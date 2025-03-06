076579f53257b72a80ce1e5ae5381c42aab9118b
Gelis-hakutaku <153464240+Gelis-hakutaku@users.noreply.github.com>
2025-03-06 12:03:14 +0100

    fixing somes issues

--- Assets/Scripts/UIScripts/LoadScene.cs
+++ Assets/Scripts/UIScripts/LoadScene.cs
@@ -5,17 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class LoadScene : MonoBehaviour
 {
-    [SerializeField] private string _sceneToLoad;
-
-    public void OnButtonClick(string scene)
+    public void OnButtonClick(string sceneToLoad)
     {
-        StartCoroutine(AsyncLoading(_sceneToLoad)); //cette coroutine permet de charger la scène sans bloquer le jeu
+        StartCoroutine(AsyncLoading(sceneToLoad)); //cette coroutine permet de charger la scène sans bloquer le jeu
     }
 
     private IEnumerator AsyncLoading(string sceneName)
     {
         if (string.IsNullOrEmpty(sceneName))
-            _sceneToLoad = SceneManager.GetActiveScene().name; //si le nom de la scène est vide, on recharge la scène actuelle
+            sceneName = SceneManager.GetActiveScene().name; //si le nom de la scène est vide, on recharge la scène actuelle
         AsyncOperation AsyncLoad = SceneManager.LoadSceneAsync(sceneName); //on lance le chargement et on stocke l'opération dans une variable
 
         //tant que le chargement n'est pas terminé, on attend
