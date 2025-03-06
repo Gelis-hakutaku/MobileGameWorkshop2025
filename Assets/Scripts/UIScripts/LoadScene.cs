d5896a8dabcc4c00a44b8b345b4eefecc12dc7c1
Gelis-hakutaku <153464240+Gelis-hakutaku@users.noreply.github.com>
2025-03-06 11:58:21 +0100

    add a pause UI and a prototype of gameplay UI

--- Assets/Scripts/UIScripts/LoadScene.cs
+++ Assets/Scripts/UIScripts/LoadScene.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -6,8 +7,21 @@ public class LoadScene : MonoBehaviour
 {
     [SerializeField] private string _sceneToLoad;
 
-    public void OnButtonClick()
+    public void OnButtonClick(string scene)
     {
-        SceneManager.LoadScene(_sceneToLoad);
+        StartCoroutine(AsyncLoading(_sceneToLoad)); //cette coroutine permet de charger la scène sans bloquer le jeu
+    }
+
+    private IEnumerator AsyncLoading(string sceneName)
+    {
+        if (string.IsNullOrEmpty(sceneName))
+            _sceneToLoad = SceneManager.GetActiveScene().name; //si le nom de la scène est vide, on recharge la scène actuelle
+        AsyncOperation AsyncLoad = SceneManager.LoadSceneAsync(sceneName); //on lance le chargement et on stocke l'opération dans une variable
+
+        //tant que le chargement n'est pas terminé, on attend
+        while (!AsyncLoad.isDone)
+        {
+            yield return null;
+        }
     }
 }
