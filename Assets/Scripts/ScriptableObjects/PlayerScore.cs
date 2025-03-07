51228fe297476448ff07c85ca0b3c27d3dbf4dc9
Gelis-hakutaku <153464240+Gelis-hakutaku@users.noreply.github.com>
2025-03-07 16:22:58 +0100

    Squashed commit of the following:
    commit d433a7cb57d6656332c543749a757a07a32e383c
    Author: Gelis-hakutaku <153464240+Gelis-hakutaku@users.noreply.github.com>
    Date:   Fri Mar 7 16:03:16 2025 +0100
    
        add dataManager GameObject and 2 script to call its functions
    
    commit 7ab6ff327269d57993a9e63fd54837df9bc5b90c
    Author: Gelis-hakutaku <153464240+Gelis-hakutaku@users.noreply.github.com>
    Date:   Fri Mar 7 15:17:07 2025 +0100
    
        add Saving functionnality
    
    commit 8eb686b953775ca07c21355dc54fd584284ff6e1
    Author: Gelis-hakutaku <153464240+Gelis-hakutaku@users.noreply.github.com>
    Date:   Fri Mar 7 09:22:47 2025 +0100
    
        Update PlayerDataManager.cs
    
    commit ab4a8ecafb0c667f50815d35010ab469d43644fe
    Author: Gelis-hakutaku <153464240+Gelis-hakutaku@users.noreply.github.com>
    Date:   Thu Mar 6 17:07:02 2025 +0100
    
        start working on saving data
    
    commit e7b65ca5c7787a460cc6dcb94c57f5d438a8336e
    Author: Gelis-hakutaku <153464240+Gelis-hakutaku@users.noreply.github.com>
    Date:   Thu Mar 6 16:42:52 2025 +0100
    
        Update Score.cs to add HighScore mechanic
    
        it need to use a save file, it is not ready yet

--- Assets/Scripts/ScriptableObjects/PlayerScore.cs
+++ Assets/Scripts/ScriptableObjects/PlayerScore.cs
@@ -1,10 +1,17 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.SceneManagement;
 
 [CreateAssetMenu(fileName = "PlayerScore", menuName = "Scriptable Objects/PlayerScore")]
 public class PlayerScore : ScriptableObject
 {
-    [SerializeField] private int _score;
+    [SerializeField] int _maxLevel;
+
+    private int _score;
+    private int[] _highscores;
+    private bool[] _isLevelLocked;
 
     public int Score
     {
@@ -12,8 +19,36 @@ public class PlayerScore : ScriptableObject
         set => _score = value;
     }
 
-    void ResetScore()
+    public int[] Highscores
+    {
+        get => _highscores;
+        set => _highscores = value;
+    }
+
+    public bool[] IsLevelLocked
+    {
+        get => _isLevelLocked;
+        set => _isLevelLocked = value;
+    }
+
+    public void Initialize()
+    {
+        _maxLevel = SceneManager.sceneCountInBuildSettings - 1;
+        _score = 0;
+        _highscores = new int[_maxLevel];
+        _isLevelLocked = new bool[_maxLevel];
+    }
+
+    public void ResetScore()
     {
         _score = 0;
+    }
+
+    public void SetHighscore(int currentLevel)
+    {
+        if (_score > _highscores[currentLevel])
+        {
+            _highscores[currentLevel] = _score;
+        }
     }
 }
