3e15158e2430e2e7f9d3033a21b0b5e0a9db486f
Fuloo <51947133+IAmJustADuck@users.noreply.github.com>
2025-03-11 13:52:24 +0100

    Mix spin & tir

--- Assets/Scripts/ShootScripts/ShootScript.cs
+++ Assets/Scripts/ShootScripts/ShootScript.cs
@@ -1,55 +1,68 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
+using static UnityEngine.GraphicsBuffer;
 
 public class ShootScript : MonoBehaviour
 {
+    [SerializeField] private float _offset;
+    [SerializeField] private float _force;
+
+    private float _mltp=(10.0f);
     private Vector2 _input;
     private Vector3 _projBaseCoord;
     private Vector3 _screenPos;
     private Vector3 _worldPos;
-    private float _mltp=(10.0f);
-    [SerializeField] private float _offset;
-    [SerializeField] private GameObject _viseur;
     private Rigidbody rb;
-    [SerializeField] private float _force;
     private Vector3 _direction;
     private Vector3 _directionNormal;
     private Vector3 _velocity;
     private float _temps;
 
+    private GameObject target;
+
     void Start()
     {
         _projBaseCoord = new Vector3(0, -1, 0);
         rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
     }
-
-    /*Vector3 Trajectoire(float t)
-    {
-        _velocity = _direction * _force;
-    }*/
-
 
     public void MoveBall()
     {
         _screenPos = new Vector3(_input.x, _input.y, 5);
         _worldPos = Camera.main.ScreenToWorldPoint(_screenPos);
-        _worldPos.z = _projBaseCoord.z + ((_input.y / Screen.height)*_mltp)-_offset;
+        _worldPos.z = _projBaseCoord.z + ((_input.y / Screen.height) * _mltp) - _offset;
         transform.position = _worldPos;
     }
 
-    public void OnClicked(InputAction.CallbackContext ctxt)
+    public void OnHold(InputAction.CallbackContext ctxt)
     {
-        rb.linearVelocity = new Vector3(0f, 0f, 0f);
-        rb.useGravity = false;
-
         _input = (ctxt.ReadValue<Vector2>());
 
-        if (_input == new Vector2(0.0f, 0.0f))
+        if (ctxt.started)
+        {
+            MoveBall();
+
+            rb.linearVelocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            rb.useGravity = false;
+
+            target = Instantiate(GameObject.CreatePrimitive(PrimitiveType.Cube), transform.position, Quaternion.identity, transform.parent);
+            target.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+        }
+
+        if (ctxt.canceled)
         {
             rb.useGravity = true;
-            _direction = _viseur.transform.position - transform.position;
+            _direction = target.transform.position - transform.position;
             _directionNormal = _direction.normalized;
-            rb.AddForce(_directionNormal * _force);
+
+            float _distance = Vector3.Distance(target.transform.position, transform.position) * 50;
+
+            rb.AddForce(_directionNormal * _force * _distance);
+
+            Destroy(target);
         }
     }
 
